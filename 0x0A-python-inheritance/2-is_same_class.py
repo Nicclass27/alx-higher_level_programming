411245fa569ee6b698ab575ea0881e08df28e8ff
nicclass27 <nicclass27@gmail.com>
2022-07-06 15:41:06 -0700

    exactly an instant

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-
-[2;2R[>77;30502;0c]10;rgb:bfbf/bfbf/bfbf]11;rgb:0000/0000/0000"""
-Check if an object is exactly an instance of the specifed class
+"""
+If Object is an instance of a specified class
 """
 
 
 def is_same_class(obj, a_class):
     """
-    Check if an object is exactly an instance of the specifed class
-    Args:
-        obj (Object): The instance to check
-        a_class (Object): The specified class
-    Returns: True of False
+     a function that returns True if the object
+     is exactly an instance of the specified class;
+     otherwise False
     """
-    return type(obj) is a_class
+    if type(obj) is a_class:
+        return True
+    return False
